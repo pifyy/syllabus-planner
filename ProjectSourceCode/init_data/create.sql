5efa46c0ca0ae1dfeb8c3328f8fce3eecf26f9ce
Alexander Morgan <sepiormon@gmail.com>
2026-03-29 20:45:36 -0600

    Expanded inital commit. Added tables for users/classes/assignments and a linker table for users and classes

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,12 +1,30 @@
+-- all table names should be changed for security reasons, however generic names for now for ease of development
 CREATE TABLE IF NOT EXISTS users (
-    username VARCHAR(15) PRIMARY KEY,
-    password VARCHAR(60) NOT NULL,
-    Enrolled VARCHAR(20) FORIGN KEY,
+    userID VARCHAR(10) PRIMARY KEY,
+    username VARCHAR(15) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL
+    --Add any settings saved for individual users
 );
 
 CREATE TABLE IF NOT EXISTS classes (
     classID VARCHAR(20) PRIMARY KEY,
     className VARCHAR(60) NOT NULL,
-    classSyllabusLocation VARCHAR(100),
+    SyllabusFileLocation VARCHAR(100)
 );
 
+CREATE TABLE IF NOT EXISTS students_to_classes (
+    classID VARCHAR(20),
+    userID VARCHAR(10),
+    PRIMARY KEY (classID, userID),
+    FOREIGN KEY (classID) REFERENCES classes(classID),
+    FOREIGN KEY (userID) REFERENCES users(userID)
+);
+
+CREATE TABLE IF NOT EXISTS assignments (
+    assignmentID INT PRIMARY KEY AUTO_INCREMENT,
+    type VARCHAR(7) NOT NULL, --exam, homework, quiz, project
+    name VARCHAR(50) NOT NULL,
+    due DATE NOT NULL,
+    classID VARCHAR(20),
+    FOREIGN KEY (classID) REFERENCES classes(classID)
+);
